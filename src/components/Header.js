2309f780d74878a526609954ead0d15681f5af79
Jonathan <jonathanmedhed@gmail.com>
2024-09-11 17:26:08 -0400

    improve header icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Header = ({ user, className, centerItem, icon, title }) => {
   const navigate = useNavigate();
@@ -8,29 +8,28 @@ const Header = ({ user, className, centerItem, icon, title }) => {
 
   console.log(pathname);
   return (
-    <header className={`header ${className || ''} `}>
-      <div className='header__inner'>
-        <div className='header__left' onClick={() => navigate('/')}>
-          {icon && <span className='header-icon'>{icon}</span>}
-          {title && <span className='header-title'>{title}</span>}
+    <header className={`header ${className || ""} `}>
+      <div className="header__inner">
+        <div className="header__left" onClick={() => navigate("/")}>
+          {icon && <span className="header-icon">{icon}</span>}
+          {title && <span className="header-title">{title}</span>}
         </div>
-        <span className={`header__switch ${'isShowMenu' ? '--hide' : ''}`}>
+        <span className={`header__switch ${"isShowMenu" ? "--hide" : ""}`}>
           {centerItem}
         </span>
-        <div className=''>
+        <div className="">
           {/**menu ? (
             menu
           ) : (
             <LanguageSwitch active={true} onClick={() => action()} />
           )*/}
         </div>
-        <Link
-          className='profile-btn'
-          to={`${pathname.includes(user.id) ? '/' : `users/${user.id}`}`}
-        >
+        <Link to={`${pathname.includes(user.id) ? "/" : `users/${user.id}`}`}>
           <i
-            class={`fa fa-${pathname.includes(user.id) ? 'home' : 'user'}`}
-            aria-hidden='true'
+            className={`fa fa-${
+              pathname.includes(user.id) ? "home" : "user"
+            } prof-btn`}
+            aria-hidden="true"
           />
         </Link>
       </div>
